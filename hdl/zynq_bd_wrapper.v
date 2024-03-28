f5461f74f40c00ceee2f08d6f9d831902322b054
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-03-28 12:42:53 -0400

    update wrapper

---
+++ hdl/zynq_bd_wrapper.v
@@ -0,0 +1,31 @@
+//Copyright 1986-2022 Xilinx, Inc. All Rights Reserved.
+//--------------------------------------------------------------------------------
+//Tool Version: Vivado v.2022.2 (win64) Build 3671981 Fri Oct 14 05:00:03 MDT 2022
+//Date        : Thu Mar 28 12:31:40 2024
+//Host        : ASCPHY-NC196428 running 64-bit major release  (build 9200)
+//Command     : generate_target zynq_bd_wrapper.bd
+//Design      : zynq_bd_wrapper
+//Purpose     : IP block netlist
+//--------------------------------------------------------------------------------
+`timescale 1 ps / 1 ps
+
+module zynq_bd_wrapper
+   (UART_1_0_rxd,
+    UART_1_0_txd,
+    GPIO_0_0_tri_i,
+    GPIO_0_0_tri_o,
+    GPIO_0_0_tri_t
+    );
+  input UART_1_0_rxd;
+  output UART_1_0_txd;
+  input [15:0] GPIO_0_0_tri_i;
+  output [15:0] GPIO_0_0_tri_o;
+  output [15:0] GPIO_0_0_tri_t;
+
+  zynq_bd zynq_bd_i
+       (.UART_1_0_rxd(UART_1_0_rxd),
+        .UART_1_0_txd(UART_1_0_txd),
+        .GPIO_0_0_tri_i(GPIO_0_0_tri_i),
+        .GPIO_0_0_tri_o(GPIO_0_0_tri_o),
+        .GPIO_0_0_tri_t(GPIO_0_0_tri_t));
+endmodule
