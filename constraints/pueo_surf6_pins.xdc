d2239e5832efe84ab5de6feb1a53ddefec162747
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-04-01 17:25:24 -0400

    add constraints

---
+++ constraints/pueo_surf6_pins.xdc
@@ -0,0 +1,25 @@
+# AG17
+
+set_property -dict { PACKAGE_PIN AG17 IOSTANDARD LVDS DIFF_TERM TRUE } [get_ports SYSREFCLK_P]
+set_property -dict { PACKAGE_PIN AH17 IOSTANDARD LVDS DIFF_TERM TRUE } [get_ports SYSREFCLK_N]
+
+set_property -dict { PACKAGE_PIN F14 IOSTANDARD LVCMOS33 } [get_ports PL_SYSREF_P]
+set_property -dict { PACKAGE_PIN F13 IOSTANDARD LVCMOS33 } [get_ports PL_SYSREF_N]
+
+set_property -dict { PACKAGE_PIN AK16 IOSTANDARD LVDS DIFF_TERM TRUE } [get_ports RXCLK_P]
+set_property -dict { PACKAGE_PIN AJ17 IOSTANDARD LVDS DIFF_TERM TRUE } [get_ports RXCLK_N]
+
+set_property -dict { PACKAGE_PIN AN18 IOSTANDARD LVDS } [get_ports TXCLK_P]
+set_property -dict { PACKAGE_PIN AP17 IOSTANDARD LVDS } [get_ports TXCLK_N]
+
+set_property -dict { PACKAGE_PIN AM16 IOSTANDARD LVDS DIFF_TERM TRUE } [get_ports CIN_P]
+set_property -dict { PACKAGE_PIN AL17 IOSTANDARD LVDS DIFF_TERM TRUE } [get_ports CIN_N]
+
+set_property -dict { PACKAGE_PIN AP16 IOSTANDARD LVDS } [get_ports COUT_P]
+set_property -dict { PACKAGE_PIN AP15 IOSTANDARD LVDS } [get_ports COUT_N]
+
+set_property -dict { PACKAGE_PIN AN17 IOSTANDARD LVDS } [get_ports DOUT_P]
+set_property -dict { PACKAGE_PIN AM17 IOSTANDARD LVDS } [get_ports DOUT_N]
+
+set_property -dict { PACKAGE_PIN D13 IOSTANDARD LVCMOS33 } [get_ports CMD_RX]
+set_property -dict { PACKAGE_PIN C13 IOSTANDARD LVCMOS33 } [get_ports CMD_TX]
---
+++ constraints/pueo_surf6_timing.tcl
@@ -0,0 +1,76 @@
+######## CONVENIENCE FUNCTIONS
+# These all have escape clauses because clocks sometimes don't exist in the elaboration/synthesis
+# steps.
+
+proc set_cc_paths { srcClk dstClk ctlist } {
+    if {$srcClk eq ""} {
+        puts "set_cc_paths: No source clock: returning."
+        return
+    }
+    if {$dstClk eq ""} {
+        puts "set_cc_paths: No destination clock: returning."
+        return
+    }
+    array set ctypes $ctlist
+    set srcType $ctypes($srcClk)
+    set dstType $ctypes($dstClk)
+    set maxTime [get_property PERIOD $srcClk]
+    set srcRegs [get_cells -hier -filter "CUSTOM_CC_SRC == $srcType"]
+    set dstRegs [get_cells -hier -filter "CUSTOM_CC_DST == $dstType"]
+    set_max_delay -datapath_only -from $srcRegs -to $dstRegs $maxTime
+}
+
+proc set_gray_paths { srcClk dstClk ctlist } {
+    if {$srcClk eq ""} {
+        puts "set_gray_paths: No source clock: returning."
+        return
+    }
+    if {$dstClk eq ""} {
+        puts "set_gray_paths: No destination clock: returning."
+        return
+    }
+    array set ctypes $ctlist
+    set maxTime [get_property PERIOD $srcClk]
+    set maxSkew [expr min([get_property PERIOD $srcClk], [get_property PERIOD $dstClk])]
+    set srcRegs [get_cells -hier -filter "CUSTOM_GRAY_SRC == $ctypes($srcClk)"]
+    set dstRegs [get_cells -hier -filter "CUSTOM_GRAY_DST == $ctypes($dstClk)"]
+    set_max_delay -datapath_only -from $srcRegs -to $dstRegs $maxTime
+    set_bus_skew -from $srcRegs -to $dstRegs $maxSkew
+}
+
+proc set_ignore_paths { srcClk dstClk ctlist } {
+    if {$srcClk eq ""} {
+        puts "set_ignore_paths: No source clock: returning."
+        return
+    }
+    if {$dstClk eq ""} {
+        puts "set_ignore_paths: No destination clock: returning."
+        return
+    }
+    array set ctypes $ctlist
+    set srcRegs [get_cells -hier -filter "CUSTOM_IGN_SRC == $ctypes($srcClk)"]
+    set dstRegs [get_cells -hier -filter "CUSTOM_IGN_DST == $ctypes($dstClk)"]
+    set_false_path -from $srcRegs -to $dstRegs
+}
+
+######## END CONVENIENCE FUNCTIONS
+
+# PIN CLOCKS
+set sysrefclk [create_clock -period 2.667 -name sysref_clk [get_ports -filter { NAME =~ "SYSREFCLK_P" && DIRECTION == "IN" }]]
+set clktypes($sysrefclk) SYSREFCLK
+
+set regclk [get_clocks -of_objects [get_nets -hier -filter { NAME =~ "regclk"}]]
+set clktypes($regclk) REGCLK
+
+# create clktypelist variable to save
+set clktypelist [array get clktypes]
+
+###### END CLOCK DEFINITIONS
+
+# autoignore the flag_sync module guys
+set sync_flag_regs [get_cells -hier -filter {NAME =~ *FlagToggle_clkA_reg*}]
+set sync_sync_regs [get_cells -hier -filter {NAME =~ *SyncA_clkB_reg*}]
+set sync_syncB_regs [get_cells -hier -filter {NAME =~ *SyncB_clkA_reg*}]
+set_max_delay -datapath_only -from $sync_flag_regs -to $sync_sync_regs 10.000
+set_max_delay -datapath_only -from $sync_sync_regs -to $sync_syncB_regs 10.000
+
