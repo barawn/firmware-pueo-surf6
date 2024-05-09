8abe09b50fc54f8dc898b63af0a17a9fe1d5f757
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2024-05-09 15:17:46 -0400

    make wrapper version-detect by parameter

--- hdl/zynq_bd_wrapper.v
+++ hdl/zynq_bd_wrapper.v
@@ -17,17 +17,31 @@ module zynq_bd_wrapper
     GPIO_0_0_tri_t,
     pl_clk0_0    
     );
+    
+  parameter REVISION = "A";    
   input UART_1_0_rxd;
   output UART_1_0_txd;
   input [15:0] GPIO_0_0_tri_i;
   output [15:0] GPIO_0_0_tri_o;
   output [15:0] GPIO_0_0_tri_t;
   output pl_clk0_0;
-  zynq_bd zynq_bd_i
-       (.UART_1_0_rxd(UART_1_0_rxd),
-        .UART_1_0_txd(UART_1_0_txd),
-        .GPIO_0_0_tri_i(GPIO_0_0_tri_i),
-        .GPIO_0_0_tri_o(GPIO_0_0_tri_o),
-        .GPIO_0_0_tri_t(GPIO_0_0_tri_t),
-        .pl_clk0_0(pl_clk0_0));
+  generate
+    if (REVISION == "B") begin : REVB
+      zynq_bd_revB zynq_bd_i
+           (.UART_1_0_rxd(UART_1_0_rxd),
+            .UART_1_0_txd(UART_1_0_txd),
+            .GPIO_0_0_tri_i(GPIO_0_0_tri_i),
+            .GPIO_0_0_tri_o(GPIO_0_0_tri_o),
+            .GPIO_0_0_tri_t(GPIO_0_0_tri_t),
+            .pl_clk0_0(pl_clk0_0));
+    end else begin : REVA
+      zynq_bd zynq_bd_i
+           (.UART_1_0_rxd(UART_1_0_rxd),
+            .UART_1_0_txd(UART_1_0_txd),
+            .GPIO_0_0_tri_i(GPIO_0_0_tri_i),
+            .GPIO_0_0_tri_o(GPIO_0_0_tri_o),
+            .GPIO_0_0_tri_t(GPIO_0_0_tri_t),
+            .pl_clk0_0(pl_clk0_0));
+     end  
+  endgenerate
 endmodule
