06714d8c60c6b1c47b6d37145d51b4177e62b6e3
U-ASC\allison.122 <allison.122@ASCPHY-NC196428.asc.ohio-state.edu>
2025-05-22 16:42:35 -0400

    drive CALEN and bump to 0.1.13

--- hdl/pueo_surf6.sv
+++ hdl/pueo_surf6.sv
@@ -16,7 +16,7 @@ module pueo_surf6 #(parameter IDENT="SURF",
                     parameter DEVICE="GEN3",
                     parameter [3:0] VER_MAJOR = 4'd0,
                     parameter [3:0] VER_MINOR = 4'd1,
-                    parameter [7:0] VER_REV = 8'd12,
+                    parameter [7:0] VER_REV = 8'd13,
                     // this gets autofilled by pre_synthesis.tcl
                     parameter [15:0] FIRMWARE_DATE = {16{1'b0}},
                     // we have multiple GTPCLK options
@@ -251,6 +251,8 @@ module pueo_surf6 #(parameter IDENT="SURF",
     assign FP_LED = idctrl_gpo[2:1];
     assign CAL_SEL_B = ~idctrl_gpo[3];
     assign SEL_CAL = idctrl_gpo[4];
+    // CALEN is !SHDN so if SEL_CAL is low, shut it off.
+    assign CALEN = !idctrl_gpo[4];
     assign SEL_CAL_B = ~idctrl_gpo[4];
     (* CUSTOM_CC_DST = "SYSREFCLK" *)
     BUFGCE #(.CE_TYPE("SYNC")) u_aclk_bufg(.I(aclk_in),.O(aclk),.CE(clocks_ready));
